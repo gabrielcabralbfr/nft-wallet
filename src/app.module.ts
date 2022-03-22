6eb6ecd34849f9f867110ad1e42f1bf2bf483d46
Gabriel Cabral <gabriel.cabral@celsolisboa.edu.br>
2022-03-22 02:12:49 -0300

    feat: rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletController } from './wallet/wallet.controller';
 import { WalletService } from './wallet/wallet.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 1,
+    }),
+  ],
   controllers: [AppController, WalletController],
-  providers: [AppService, WalletService],
+  providers: [AppService, WalletService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }],
 })
 export class AppModule { }
